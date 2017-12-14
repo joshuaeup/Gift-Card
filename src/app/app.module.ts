cf0be085306926c97b15ad5718179dbc53df9809
Josh <joshuaeup@gmail.com>
2017-12-14 13:24:10 -0500

    established routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }          from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './components/Home/app.component';
+import { About } from "./components/About/about";
+import { Home } from "./components/HomeOne/home";
 
 const appRoutes: Routes = [
-  // { path: 'crisis-center', component:  },
-  // { path: 'hero/:id',      component:  },
-  // {
-  //   path: 'heroes',
-  //   component: ,
-  //   data: { title: 'Heroes List' }
-  // },
-  // { path: '',
-  //   redirectTo: '/heroes',
-  //   pathMatch: 'full'
-  // },
-  // { path: '**', component:  }
+  { path: 'about', component: About },
+  { path: 'home', component: Home },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    About,
+    Home
   ],
   imports: [
+    BrowserModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } 
-    ),
-    BrowserModule
+    )
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/About/about.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.html',
+  styleUrls: ['./about.css']
+})
+export class About {
+  title = 'app';
+}
